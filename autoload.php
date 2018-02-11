f00f40592ddccd757d8aacc0ea5a8499576e96da
625801 <625801@gmail.com>
2018-02-11 08:22:18 -0600

    changes path in autoload.php

--- autoload.php
+++ autoload.php
@@ -3,6 +3,6 @@ require_once(__DIR__.'/functions.php');
 require_once(__DIR__.'/ClassLoader.php');
 
 $loader = new \IL\ClassLoader();
-$loader->add(__DIR__);
+$loader->add(__DIR__."/..");
 $loader->register();
 unset($loader);
